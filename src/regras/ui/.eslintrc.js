7aa43ac7799936885ad65d8e7d6123b004966d55
vazvdr <azevedo.vanderson@hotmail.com>
2025-01-09 21:00:42 -0300

    Changing eslintrc

--- src/regras/ui/.eslintrc.js
+++ src/regras/ui/.eslintrc.js
@@ -1,10 +1,26 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/react-internal.js"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "next/core-web-vitals"
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "./tsconfig.lint.json",
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: "./tsconfig.json", // Use o tsconfig principal
     tsconfigRootDir: __dirname,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
